c957bc8fbdf7b3e3cd1ae229b243577832af0319
ArletheMora <armomorali@ittepic.edu.mx>
2022-11-10 14:34:28 -0700

    inputs validados

--- src/app/new-student/new-student.page.ts
+++ src/app/new-student/new-student.page.ts
@@ -21,16 +21,62 @@ export class NewStudentPage implements OnInit {
     this.myForm = this.fb.group(
       {
         //valor inicial, validaciones
-        controlnumber: ["", Validators.compose([Validators.required, Validators.minLength(8), Validators.maxLength(8), Validators.pattern('^[0-9]+$')])]
+        controlnumber: ["", Validators.compose([Validators.required, Validators.minLength(8), Validators.maxLength(8), Validators.pattern('^[0-9]+$')])],
+        age: ["", Validators.compose([Validators.required, Validators.minLength(2), Validators.maxLength(2), Validators.pattern('^[0-9]+$'),  Validators.min(17)])],
+        carreer: ["", Validators.compose([Validators.required, Validators.pattern('')])],
+        curp: ["", Validators.compose([Validators.required, Validators.minLength(18), Validators.maxLength(18), Validators.pattern('^[a-zÑñA-Z]+[a-zÑñA-Z0-9]*$')])],
+        email: ["", Validators.compose([Validators.required, Validators.pattern(/^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i)])],
+        name: ["", Validators.compose([Validators.required, Validators.pattern('^[a-zÑñA-Z]+[a-zÑñA-Z ]*$')])],
+        nip: ["", Validators.compose([Validators.required, Validators.minLength(2), Validators.maxLength(4), Validators.pattern('[1-9][0-9]+')])],
+        photo: ["", Validators.compose([Validators.required, Validators.pattern(/^(?:([A-Za-z]+):)?(\/{0,3})([0-9.\-A-Za-z]+)(?::(\d+))?(?:\/([^?#]))?(?:\?([^#]))?(?:#(.*))?$/)])]
       }
     );
 
     this.validationMessages = {
       controlnumber: [
-        {type: 'required', message: "Número de control obligatorio"},
-        {type: 'minlength', message: "Debe ser de 8 dígitos"},
-        {type: 'maxlength', message: "Debe ser de 8 dígitos"},
-        {type: 'pattern', message: "EL número de control está mal formado"}
+        { type: 'required', message: "Número de control obligatorio" },
+        { type: 'minlength', message: "Debe ser de 8 dígitos" },
+        { type: 'maxlength', message: "Debe ser de 8 dígitos" },
+        { type: 'pattern', message: "EL número de control está mal formado" }
+      ],
+      age: [
+        { type: 'required', message: "Edad obligatoria" },
+        { type: 'minlength', message: "Debe se de 2 dígitos" },
+        { type: 'maxlength', message: "Debe ser de 2 dígitos" },
+        { type: 'pattern', message: "La edad está mal formada" },
+        { type: 'min', message: "Edad mínima 17" },
+      ],
+      carreer: [
+        { type: 'required', message: "Carrera obligatorio" }
+      ]
+      ,
+      curp: [
+        { type: 'required', message: "Nombre obligatorio" },
+        { type: 'minlength', message: "Debe ser de 18 dígitos" },
+        { type: 'maxlength', message: "Debe ser de 18 dígitos" },
+        { type: 'pattern', message: "La CURP está mal formada" }
+      ]
+      ,
+      email: [
+        { type: 'required', message: "Email obligatorio" },
+        { type: 'pattern', message: "EL email está mal formado" }
+      ]
+      ,
+      name: [
+        { type: 'required', message: "Nombre obligatorio" },
+        { type: 'pattern', message: "EL nombre está mal formado" }
+      ]
+      ,
+      nip: [
+        { type: 'required', message: "Nombre obligatorio" },
+        { type: 'minlength', message: "Debe ser de al menos 2 dígitos dígitos" },
+        { type: 'maxlength', message: "Debe ser de máximo 4 dígitos" },
+        { type: 'pattern', message: "EL nip está mal formado" }
+      ]
+      ,
+      photo: [
+        { type: 'required', message: "Enlace de foto obligatorio" },
+        { type: 'pattern', message: "EL enlace de la foto está mal formado" }
       ]
     }
   }
@@ -40,6 +86,6 @@ export class NewStudentPage implements OnInit {
     this.studentService.newStudent(this.student);
   }
 
-  
+
 
 }
